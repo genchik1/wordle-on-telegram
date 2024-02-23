7a19ed56979d94acd33dd743feae42561f8f44cc
Gennadiy Matveev <geno.matveev@gmail.com>
2024-02-23 14:18:23 +0400

    adds erudajs

--- frontend/src/api/WordsAPI.jsx
+++ frontend/src/api/WordsAPI.jsx
@@ -1,13 +1,8 @@
 import axios from "axios";
-import {apiDomain, tgUser} from "../Constants.jsx";
+import {apiDomain} from "../Constants.jsx";
 
 export async function getTodayWord() {
     let link = `${apiDomain}/api/words/today`;
-    const response = await axios.get(link, {
-        params: {
-            user_id: tgUser,
-            //init_data:  window.Telegram.WebApp.initData,
-        },
-    });
+    const response = await axios.get(link);
     return response.data;
 }
